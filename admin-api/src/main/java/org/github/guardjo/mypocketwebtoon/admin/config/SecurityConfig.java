fa99ccf966cc986a730024d18a94be88053a8625
Kyeongho <dagh1218@naver.com>
2026-03-15 19:38:50 +0900

    [BO] #1 PR(#6) 피드백 반영
    - prod 프로파일용 설정 파일 추가
    - testcontainer postgres 버전 지정

--- admin-api/src/main/java/org/github/guardjo/mypocketwebtoon/admin/config/SecurityConfig.java
+++ admin-api/src/main/java/org/github/guardjo/mypocketwebtoon/admin/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // TODO 추후 인증/인가 필터링 구성 에정
         httpSecurity.authorizeHttpRequests(registry -> {
             registry.anyRequest().permitAll();
         });
