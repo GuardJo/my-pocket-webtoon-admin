3a6f7eac7fdf73e8252c6be92442b378c0368e5a
Kyeongho <dagh1218@naver.com>
2026-05-05 16:23:52 +0900

    [BO] #21 특정 작품 정보 조회 엔드포인트 구성
    - 작품 상세 내역 조회를 위한 vo 객체 구성
    - 특정 작품 조회 엔드포인트 구성

--- admin-api/src/main/java/org/github/guardjo/mypocketwebtoon/admin/api/docs/WorkApiDocs.java
+++ admin-api/src/main/java/org/github/guardjo/mypocketwebtoon/admin/api/docs/WorkApiDocs.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.github.guardjo.mypocketwebtoon.admin.model.request.WorkUploadRequest;
 import org.github.guardjo.mypocketwebtoon.admin.model.response.BaseResponse;
+import org.github.guardjo.mypocketwebtoon.admin.model.vo.WorkInfo;
 import org.github.guardjo.mypocketwebtoon.admin.model.vo.WorkSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,4 +17,7 @@ public interface WorkApiDocs {
 
     @Operation(summary = "작품 목록 조회", description = "현재 등록된 작품 목록을 조회한다.")
     BaseResponse<Page<WorkSummary>> getWorks(@Parameter(name = "페이지 처리") Pageable pageable);
+
+    @Operation(summary = "특정 작품 조회", description = "식별키에 해당하는 작품 정보를 조회한다.")
+    BaseResponse<WorkInfo> getWorkInfo(@Parameter(name = "작품 식별키") Long workId);
 }
---
+++ admin-api/src/main/java/org/github/guardjo/mypocketwebtoon/admin/model/vo/WorkInfo.java
@@ -0,0 +1,32 @@
+package org.github.guardjo.mypocketwebtoon.admin.model.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDate;
+
+/*
+작품 정보
+ */
+public record WorkInfo(
+        @Schema(description = "작품 식별키", example = "999")
+        Long id,
+
+        @Schema(description = "작품 대표썸네일 이미지 URL", example = "https://cdn.com/thumnbnail.jpg")
+        String thumbnailUrl,
+
+        @Schema(description = "작품 연재상태", example = "COMPLETED")
+        String serialState,
+
+        @Schema(description = "작품명", example = "웹툰명")
+        String title,
+
+        @Schema(description = "작품 설명", example = "설명")
+        String description,
+
+        @Schema(description = "회차 수", example = "100")
+        int episodeTotalSize,
+
+        @Schema(description = "마지막 업데이트 일자", example = "yyyy.MM.dd")
+        LocalDate lastUpdateDate
+) {
+}
